80b38b1b7b398ddfb4da2ef99c24819150e8e110
pookzzz <wanyizhou.z@gmail.com>
2020-02-08 01:19:24 +0800

    Fixing zsh syntax highlighting plugin

--- setup.sh
+++ setup.sh
@@ -37,3 +37,4 @@ ln -s -f $repo/.tmux.conf $HOME/.tmux.conf
 # ZSH config
 git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
