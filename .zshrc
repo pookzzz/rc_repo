6389aa17d50b533687bc01c63bd2f2d99f21a390
pookzzz <wanyizhou.z@gmail.com>
2023-09-12 15:16:56 +0800

    updating mac rc configs

--- .zshrc
+++ .zshrc
@@ -17,7 +17,7 @@ export ZSH=$HOME/.oh-my-zsh
 # it'll load a random theme each time that oh-my-zsh is loaded.
 # See https://github.com/robbyrussell/oh-my-zsh/wiki/Themes
 #ZSH_THEME="random"
-ZSH_THEME="powerlevel9k/powerlevel9k"
+ZSH_THEME="random"
 
 # Uncomment the following line to use case-sensitive completion.
 CASE_SENSITIVE="true"
@@ -69,7 +69,7 @@ plugins=(
     battery
     zsh-syntax-highlighting
     zsh-autosuggestions
-    osx
+    macos
     gatsby    
 )
 
---
+++ setup.sh
@@ -0,0 +1,32 @@
+# Backing up existing configs
+mkdir -p $HOME/backup_config
+backup="$HOME/backup_config"
+
+# Making Directory tree for vim plugins
+mkdir -p $HOME/.vim/autoload
+curl -flo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+mv $HOME/.vimrc $backup
+mv $HOME/.aliases* $backup
+mv $HOME/.zshrc $backup
+mv $HOME/.tmux.conf $backup
+
+echo "This script needs to be ran inside the rc_po directory."
+repo="`pwd`"
+echo $repo
+echo "Linking vim"
+ln -s -f $repo/.vimrc $HOME/.vimrc
+ln -s -f $repo/.vimpressrc $HOME/.vimpressrc
+echo "Linking zshrc"
+ln -s -f $repo/.zshrc $HOME/.zshrc
+ln -s -f $repo/.aliases_zsh $HOME/.aliases_zsh
+echo "Linking tmux"
+ln -s -f $repo/.tmux.conf $HOME/.tmux.conf
+
+# ZSH config
+git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+
+# Setup workspace
+# TODO
